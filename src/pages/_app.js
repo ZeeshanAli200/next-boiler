5867ea1d4614924569aee237a6fc4b5b9574a618
zeeshan-dev <>
2023-02-21 05:34:46 +0500

    issues

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,32 @@
 // import 'antd/dist/antd.css'
 import store from '@/store/store'
 import '@/styles/globals.css'
+import { ConfigProvider } from 'antd'
 import { Provider } from 'react-redux'
+import variables from '@/styles/variables.module.scss'
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: variables.primaryText,
+            colorPrimaryBgHover: variables.secondaryText,
+            colorPrimaryHover: variables.secondaryText,
+            colorPrimaryActive: variables.primaryText,
+            colorPrimaryBg: variables.bgPrimary,
+          },
+        }}
+      >
+        <div class="wrapper">
+          <header>I'm a 30px tall header</header>
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <footer>I'm a 30px tall footer</footer>
+        </div>
+      </ConfigProvider>
     </Provider>
   )
 }
